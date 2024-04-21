b979c43ed83660e4633e547c63a95cce4ddee53a
Kamil Kobylarz <xraxus@gmail.com>
2024-04-21 15:43:32 +0200

    add initialMessages

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,8 +2,17 @@ import { createContext, useState } from "react";
 
 export const ChatContext = createContext({});
 
+const initialMessages = [
+  {
+    id: 1,
+    role: "assistant",
+    text: "Hello! I'm Kamil's AI assistant. How can I help you today?",
+    date: new Date(),
+  },
+];
+
 export default function ({ children }) {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(initialMessages);
 
   return (
     <div className="chat">

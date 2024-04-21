9efbad342f7aa0c9c3abbfc2faa77237c649b174
Kamil Kobylarz <xraxus@gmail.com>
2024-04-21 18:12:53 +0200

    create assistantHandler & fetch response

---
+++ netlify/functions/assistantHandler.js
@@ -0,0 +1,99 @@
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import {
+  RunnableSequence,
+  RunnablePassthrough,
+} from "@langchain/core/runnables";
+import formatConvHistory from "./utils/formatConvHistory";
+
+const openAIApiKey = process.env.OPENAI_API_KEY;
+const supabaseApiKey = process.env.SUPABASE_API_KEY;
+const supabbaseUrl = process.env.SUPABASE_URL;
+
+const llm = new ChatOpenAI({
+  model: "gpt-3.5-turbo",
+  openAIApiKey,
+  temperature: 0.3,
+});
+const embeddings = new OpenAIEmbeddings({
+  openAIApiKey,
+});
+const client = createClient(supabbaseUrl, supabaseApiKey);
+
+const vectorStore = new SupabaseVectorStore(embeddings, {
+  client,
+  tableName: "personal_info",
+  queryName: "match_personal_info",
+});
+
+const retriever = vectorStore.asRetriever();
+
+function combineDocuments(docs) {
+  return docs.map((doc) => doc.pageContent).join("\n###\n");
+}
+
+async function getAssistantResponse(userInput, messagesHistory) {
+  const standaloneQuestionTemplate = `Given some conversation history (if any) and a question, convert the user input to a standalone question. 
+conversation history: {conv_history}
+user input: {user_input} 
+standalone question:`;
+  const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
+    standaloneQuestionTemplate
+  );
+
+  const answerTemplate = `You are "Doxie Assistant", a helpful and enthusiastic AI assistant who can talk with the user & answer a given question about me - Kamil Kobylarz - based on the context provided and the conversation history.
+  If user input was not a question, answer to mantain conversation, If user input is a question try to find the answer in the context and conversation history. 
+  If the answer to question is not given in the context, find the answer in the conversation history if possible. If user input was a question and you  don't know the answer, say that you dont know.  Don't try to make up an answer. Always speak as if you were chatting to a good friend.
+
+  conversation history: {conv_history}
+  context: {context}
+  user input: {user_input} 
+  answer: `;
+  const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
+
+  const standaloneQuestionChain = standaloneQuestionPrompt
+    .pipe(llm)
+    .pipe(new StringOutputParser());
+
+  const retrieverChain = RunnableSequence.from([
+    (prevResult) => prevResult.standalone_question,
+    retriever,
+    combineDocuments,
+  ]);
+
+  const answerChain = answerPrompt.pipe(llm).pipe(new StringOutputParser());
+
+  const chain = RunnableSequence.from([
+    {
+      standalone_question: standaloneQuestionChain,
+      original_input: new RunnablePassthrough(),
+    },
+    {
+      context: retrieverChain,
+      user_input: ({ original_input }) => original_input.user_input,
+      conv_history: ({ original_input }) => original_input.conv_history,
+    },
+    answerChain,
+  ]);
+
+  const response = await chain.invoke({
+    user_input: userInput.text,
+    conv_history: formatConvHistory(messagesHistory),
+  });
+  return response;
+}
+export default async function handler(event, context) {
+  const body = JSON.parse(event.body);
+
+  const { userInput, messagesHistory } = body;
+
+  const response = await getAssistantResponse(userInput, messagesHistory);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  };
+}
--- src/components/Chat/ChatInput.jsx
+++ src/components/Chat/ChatInput.jsx
@@ -13,10 +13,19 @@ export default function ChatInput() {
     setMessages((prevMessages) => [...prevMessages, newMessage]);
     setInputMessage("");
     try {
-      const assistantResponse = await getAssistantResponse(
-        newMessage,
-        messages
-      );
+      const assistantResponse = await fetch(
+        "/.netlify/functions/assistantHandler",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userInput: newMessage,
+            messagesHistory: messages,
+          }),
+        }
+      ).then((response) => response.json());
       setMessages((prevMessages) => [
         ...prevMessages,
         createNewMessage(assistantResponse, "assistant"),
